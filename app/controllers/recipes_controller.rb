fde62b3de73fd1f9b4ac86cfdca4d673e9eeeaa2
hm0913 <75556338+hm0913@users.noreply.github.com>
2021-03-06 15:52:08 +0900

    レシピの編集・削除機能の実装

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -14,9 +14,39 @@ class RecipesController < ApplicationController
   end
 
   def create
-    Recipe.create(recipe_params)
-    redirect_to root_path
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to user_path(current_user[:id]), notice: "メニューを投稿しました。"
+    else
+      redirect_to new_recipe_path, alert: "投稿に失敗しました。"
+    end
   end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+    @categorys = Category.all
+    @orders = Order.all
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(update_recipe_params)
+      redirect_to root_path, notice: "メニュー内容を編集しました。"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    if @recipe.destroy
+      redirect_to root_path, notice: "メニューを削除しました。"
+    else
+      render :edit
+    end
+  end
+
 
 
   private
@@ -24,6 +54,13 @@ class RecipesController < ApplicationController
     params.require(:recipe).permit(:name, :category_id, :image, :serving, :introduction, 
       ingredients_attributes:[:name, :amount, :recipe_id, :_destroy],
       makes_attributes:[:sentence, :recipe_id, :order_id, :_destroy]
+    ).merge(user_id: current_user.id, )
+  end
+
+  def update_recipe_params
+    params.require(:recipe).permit(:name, :category_id, :image, :serving, :introduction, 
+      ingredients_attributes:[:name, :amount, :recipe_id, :_destroy, :id],
+      makes_attributes:[:sentence, :recipe_id, :order_id, :_destroy, :id]
     ).merge(user_id: current_user.id, )
   end
 end
