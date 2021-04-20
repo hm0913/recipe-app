b8041eb9573dff9c358175e23f50faaa82094426
hm0913 <75556338+hm0913@users.noreply.github.com>
2021-04-20 14:30:00 +0900

    carrierwave.rbの修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -16,6 +16,6 @@ CarrierWave.configure do |config|
     }
 
     config.fog_directory  = 'recipe-app2021'
-    config.asset_host = 'https://recipe-app2021.s3-ap-northeast-1.amazonaws.com'
+    config.asset_host = 'https://recipe-app2021.s3.amazonaws.com'
   end
 end
